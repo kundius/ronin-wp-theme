8baeb966b83875dce944017210015e691ecf93e5
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-03-16 15:20:40 +0300

    123

--- templates/home.php
+++ templates/home.php
@@ -248,6 +248,10 @@ $news = new WP_Query([
         <div class="activity-tabs">
           <div class="activity-tabs__nav">
             <label class="activity-tabs__label">
+              <input type="radio" name="activity-tabs">
+              Новости
+            </label>
+            <label class="activity-tabs__label">
               <input type="radio" name="activity-tabs" checked>
               Фотоотчёты
             </label>
@@ -255,13 +259,44 @@ $news = new WP_Query([
               <input type="radio" name="activity-tabs">
               Видео
             </label>
-            <label class="activity-tabs__label">
-              <input type="radio" name="activity-tabs">
-              Новости
-            </label>
           </div>
 
           <div class="activity-tabs__content">
+            <?php if ($news->have_posts()): ?>
+              <div class="activity-tabs__panel">
+                <div class="activity-embla" data-activity-embla>
+                  <div class="activity-embla__viewport" data-activity-embla-viewport>
+                    <div class="activity-embla__container">
+                      <?php while ($news->have_posts()): ?>
+                        <?php $news->the_post(); ?>
+                        <div class="activity-embla__slide">
+                          <?php get_template_part('partials/activity-card'); ?>
+                        </div>
+                      <?php endwhile; ?>
+                    </div>
+                  </div>
+                  <div class="activity-embla__footer">
+                    <div class="activity-embla__controls" data-activity-embla-controls>
+                      <button class="activity-embla__control" type="button" data-activity-embla-controls-prev>
+                        <span class="icon icon-arrow-left"></span>
+                      </button>
+                      <button class="activity-embla__control" type="button" data-activity-embla-controls-next>
+                        <span class="icon icon-arrow-right"></span>
+                      </button>
+                    </div>
+                    <?php if ($news->found_posts > $activity_per_page): ?>
+                      <a href="<?php echo get_term_link($news_cat); ?>" class="btn-more">
+                        <span class="btn-more__inner">
+                          <span class="btn-more__text">Смотреть все</span>
+                          <span class="btn-more__icon icon icon-arrow-right"></span>
+                        </span>
+                      </a>
+                    <?php endif; ?>
+                  </div>
+                </div>
+              </div>
+            <?php endif; ?>
+            <?php wp_reset_postdata(); ?>
             <?php if ($photo_reports->have_posts()): ?>
               <div class="activity-tabs__panel">
                 <div class="activity-embla" data-activity-embla>
@@ -323,41 +358,6 @@ $news = new WP_Query([
                     </div>
                     <?php if ($video_reports->found_posts > $activity_per_page): ?>
                       <a href="/video_report" class="btn-more">
-                        <span class="btn-more__inner">
-                          <span class="btn-more__text">Смотреть все</span>
-                          <span class="btn-more__icon icon icon-arrow-right"></span>
-                        </span>
-                      </a>
-                    <?php endif; ?>
-                  </div>
-                </div>
-              </div>
-            <?php endif; ?>
-            <?php wp_reset_postdata(); ?>
-            <?php if ($news->have_posts()): ?>
-              <div class="activity-tabs__panel">
-                <div class="activity-embla" data-activity-embla>
-                  <div class="activity-embla__viewport" data-activity-embla-viewport>
-                    <div class="activity-embla__container">
-                      <?php while ($news->have_posts()): ?>
-                        <?php $news->the_post(); ?>
-                        <div class="activity-embla__slide">
-                          <?php get_template_part('partials/activity-card'); ?>
-                        </div>
-                      <?php endwhile; ?>
-                    </div>
-                  </div>
-                  <div class="activity-embla__footer">
-                    <div class="activity-embla__controls" data-activity-embla-controls>
-                      <button class="activity-embla__control" type="button" data-activity-embla-controls-prev>
-                        <span class="icon icon-arrow-left"></span>
-                      </button>
-                      <button class="activity-embla__control" type="button" data-activity-embla-controls-next>
-                        <span class="icon icon-arrow-right"></span>
-                      </button>
-                    </div>
-                    <?php if ($news->found_posts > $activity_per_page): ?>
-                      <a href="<?php echo get_term_link($news_cat); ?>" class="btn-more">
                         <span class="btn-more__inner">
                           <span class="btn-more__text">Смотреть все</span>
                           <span class="btn-more__icon icon icon-arrow-right"></span>
