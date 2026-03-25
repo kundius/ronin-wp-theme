3ff93acc453306ee54bcabe4ed4b34cea4a81262
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-03-25 13:29:26 +0300

    123

--- partials/directions-descriptions.php
+++ partials/directions-descriptions.php
@@ -3,23 +3,19 @@
   <div class="directions-descriptions__bg"></div>
   <div class="directions-descriptions__layout">
     <div class="directions-descriptions__left">
-      <div class="directions-description-left">
-        <div class="directions-description-left__title">
-          <?php echo nl2br($descriptions[0]['title']); ?>
-        </div>
-        <div class="directions-description-left__content">
-          <?php echo $descriptions[0]['content']; ?>
-        </div>
+      <div class="directions-descriptions__title">
+        <?php echo nl2br($descriptions[0]['title']); ?>
+      </div>
+      <div class="directions-descriptions__content">
+        <?php echo $descriptions[0]['content']; ?>
       </div>
     </div>
     <div class="directions-descriptions__right">
-      <div class="directions-description-right">
-        <div class="directions-description-right__title">
-          <?php echo nl2br($descriptions[0]['title']); ?>
-        </div>
-        <div class="directions-description-right__content">
-          <?php echo $descriptions[0]['content']; ?>
-        </div>
+      <div class="directions-descriptions__title">
+        <?php echo nl2br($descriptions[0]['title']); ?>
+      </div>
+      <div class="directions-descriptions__content">
+        <?php echo $descriptions[0]['content']; ?>
       </div>
     </div>
   </div>
