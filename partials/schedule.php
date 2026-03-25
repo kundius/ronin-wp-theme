a7b5941d9ae424d4ad8976c3fa41f1bfdfb0e440
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-03-25 18:48:13 +0300

    123

--- partials/schedule.php
+++ partials/schedule.php
@@ -57,6 +57,78 @@
         <?php endforeach; ?>
       </div>
     </div>
+    <div class="elidehcs-table">
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td">
+            <div><?php echo nl2br($item['time']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+          <div>Понедельник</div>
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td" data-time="<?php echo esc_attr($item['time']); ?>">
+            <div><?php echo nl2br($item['monday']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+          <div>Вторник</div>
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td" data-time="<?php echo esc_attr($item['time']); ?>">
+            <div><?php echo nl2br($item['tuesday']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+          <div>Среда</div>
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td" data-time="<?php echo esc_attr($item['time']); ?>">
+            <div><?php echo nl2br($item['wednesday']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+          <div>Четверг</div>
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td" data-time="<?php echo esc_attr($item['time']); ?>">
+            <div><?php echo nl2br($item['thursday']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+          <div>Пятница</div>
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td" data-time="<?php echo esc_attr($item['time']); ?>">
+            <div><?php echo nl2br($item['friday']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <div class="elidehcs-table__column">
+        <div class="elidehcs-table__th">
+          <div>Суббота</div>
+        </div>
+        <?php foreach ($schedule as $item): ?>
+          <div class="elidehcs-table__td" data-time="<?php echo esc_attr($item['time']); ?>">
+            <div><?php echo nl2br($item['saturday']); ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
     <div class="schedule-block__order">
       <button type="button" class="btn-primary" data-callback-button>Записаться на&nbsp;занятие</button>
     </div>
